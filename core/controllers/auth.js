a18802efb8d8b7ea2f701a7c1fb9a20180c97ecd
sduque-esencia <sduque@esenciaestudios.com>
2025-02-14 14:16:22 +0100

    auth

--- core/controllers/auth.js
+++ core/controllers/auth.js
@@ -1,18 +1,33 @@
+import bcrypt from 'bcrypt';
+import { UserModel } from "../models/index.js";
 import { ResponseHelper as response } from "../helpers/response.js";
 
 const loginUser = async (event) => {
   try {
     const { username, password } = JSON.parse(event.body);
 
-    if (username === "admin" && password === "admin123") {
-      return response.success("Login sussesful");
+    const user = await UserModel.findOne({
+      where: { username }
+    });
+
+    if (!user) {
+      return response.unauthorized({
+        message: "Credenciales incorrectas"
+      });
     }
 
-    return response.unauthorized({
-      message: "Credenciales incorrectas"
-    });
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return response.unauthorized({
+        message: "Credenciales incorrectas"
+      });
+    }
+
+    return response.success("Login successful");
+
   } catch (error) {
-    return response.badRequest(error);
+    return response.badRequest("ERROR:" + error);
   }
 };
 
