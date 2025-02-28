26054fb6d71f67f267c8729f7f496e2048009eae
Jesus Manuel Varela Miranda <jesusvarela@esenciaestudios.com>
2025-02-28 00:17:51 -0500

    fix: ajustes en la implementación de arquitectura corrigiendo detalles y solución a la integración con cognito sin necesidad de usar el IAM.

--- core/services/authService.js
+++ core/services/authService.js
@@ -1,10 +1,36 @@
-import * as authRpository from '../repositories/authRepository.js';
+import { changePasswordRequiredInCognito, loginUserAWS } from '../helpers/aws.js';
+import * as authRepository from '../repositories/authRepository.js';
 
 
 export const login = async (loginData) => {
-    return await authRpository.login(loginData);
+    try {
+        const { username, password } = loginData;
+        
+        const userExistsInDB = await authRepository.checkUserInDatabase({ username, password });
+        const authResponse = await loginUserAWS(username, password);
+    
+        if (authResponse.ChallengeName === 'NEW_PASSWORD_REQUIRED') {
+            return { session: authResponse.Session, forceChangePassword: true };
+        }
+    
+        return {
+            token: authResponse.AuthenticationResult,
+            user: userExistsInDB.dataValues,
+        };
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
 }
 
 export const forceChangePassword = async (data) => {
-    return await authRpository.forceChangePassword(data);
+    try {
+        const { username, session, newPassword } = data;
+        await authRepository.checkUserInDatabase({ username });
+        await changePasswordRequiredInCognito({ username, newPassword, session });
+        await authRepository.changePasswordInDB({ username, newPassword });
+        return "Password changed successfully";
+    } catch (error) {
+        throw error;
+    }
 }
