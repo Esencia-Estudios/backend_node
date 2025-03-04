e047859d9d3c390d1cbe586b021ef1e5d7d21a23
sduque-esencia <sduque@esenciaestudios.com>
2025-03-04 12:02:39 +0100

    create log

--- core/services/authService.js
+++ core/services/authService.js
@@ -1,18 +1,27 @@
 import { changePasswordRequiredInCognito, loginUserAWS } from '../helpers/aws.js';
 import * as authRepository from '../repositories/authRepository.js';
+// import { AccessLog } from '../models/index.js';
 
 
 export const login = async (loginData) => {
     try {
         const { username, password } = loginData;
-        
+
         const userExistsInDB = await authRepository.checkUserInDatabase({ username, password });
         const authResponse = await loginUserAWS(username, password);
-    
+
         if (authResponse.ChallengeName === 'NEW_PASSWORD_REQUIRED') {
             return { session: authResponse.Session, forceChangePassword: true };
         }
-    
+
+        // const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+        // await AccessLog.create({
+        //     userId: userExistsInDB.id,
+        //     ipAddress: ip,
+        //     accessedAt: new Date(),
+        // });
+
         return {
             token: authResponse.AuthenticationResult,
             user: userExistsInDB.dataValues,
