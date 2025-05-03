c1573d2dce910ae0e92355701dc3546d7c5dd220
Jose orozco <josemiguelorozcomartinez687@gmail.com>
2025-05-02 19:07:15 -0500

    git push

---
+++ core/handlers/subscriptionHandlers.js
@@ -0,0 +1,17 @@
+import {
+    getPlans, getModules,
+    getSubscriptions,
+    getSubscriptionById,
+    createSubscription,
+    updateSubscription,
+    deleteSubscription,
+} from "../controllers/subscriptionController.js";
+
+export {
+    getPlans, getModules,
+    getSubscriptions,
+    getSubscriptionById,
+    createSubscription,
+    updateSubscription,
+    deleteSubscription,
+};
